8fc60a7807e752740dc901db08fa8bccef5b5a42
Aditi Lal <139861603+lal15aditi@users.noreply.github.com>
2024-03-06 19:41:18 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,38 @@
-import React from 'react'
+import React, { useState } from "react";
 
 const App = () => {
+  const [name, setName] = useState("");
+  const [arr, setArr] = useState(["list"]);
+
+  function handle(e) {
+    e.preventDefault();
+    setArr([...arr, name]);
+  }
   return (
     <div id="main">
-               {/* Do not remove the main div */}
+      <form onSubmit={handle}>
+        <input
+          type="text"
+          name="relativeName"
+          onChange={(e) => setName(e.target.value)}
+        />
+        <button type="submit">Submit</button>
+      </form>
+      <ol id="relativeList" key="relativeList">
+        {arr.length >= 1 &&
+          arr.map((item, index) => {
+            return (
+              <li
+                id={`relativeListItem${index + 1}`}
+                key={`relativeListItem${index + 1}`}
+              >
+                {item}
+              </li>
+            );
+          })}
+      </ol>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
